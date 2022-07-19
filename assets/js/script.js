719fc577dcf158f32518ad187a9082a69c2c6215
Brian Gee <briangeertsma@gmail.com>
2022-07-19 19:21:21 -0400

    fully functional for current day results

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+const OPEN_WEATHER_API_KEY = "49160125f6ab2cf7e612c72cdd9d0e00";
+const CITY_SEARCH_TEXT_AREA = document.querySelector("#city-search");
+const CITY_SEARCH_BUTTON = document.querySelector("#city-button");
+const CITY_NAME = document.querySelector("#city-name");
+const TEMP = document.querySelector("#temp");
+const WIND = document.querySelector("#wind");
+const HUMIDITY = document.querySelector("#humidity");
+const UV_INDEX = document.querySelector("#uv-index");
+
+CITY_SEARCH_BUTTON.addEventListener("click", () => {
+    fetchLatLong(CITY_SEARCH_TEXT_AREA.value);
+})
+CITY_SEARCH_TEXT_AREA.addEventListener("keyup", (e) => {
+    if (e.keyCode == 13) {
+        // fetchLatLong(CITY_SEARCH_TEXT_AREA.value);
+        CITY_SEARCH_BUTTON.click();
+    }
+})
+
+function fetchWeather(lat, lon) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&exclude=hourly,minutely&lat=" + lat + "&lon=" + lon + "&appid=" + OPEN_WEATHER_API_KEY;
+    fetch(queryURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data){
+            TEMP.textContent = ("Temp: " + data.current.temp + "°F");
+            WIND.textContent = ("Wind: " + data.current.wind_speed + " MPH");
+            HUMIDITY.textContent = ("Humidity: " + data.current.humidity + "%")
+            UV_INDEX.textContent = ("UV Index: " + data.current.uvi)
+            console.log(data);
+
+        })
+}
+
+function fetchLatLong(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + OPEN_WEATHER_API_KEY;
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data){
+            var lat = data.coord.lat;
+            var long = data.coord.lon;
+            CITY_NAME.textContent = (data.name + " " + data.dt);
+            fetchWeather(lat, long);
+
+
+        })
+}
